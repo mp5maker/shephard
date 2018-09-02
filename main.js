72453dbe9b171463634fada8c8856d344d37f579
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-09-02 13:14:02 +0600

    Adding Dry Concept

--- main.js
+++ main.js
@@ -38,130 +38,60 @@
                 scrollTo: true,
             }
         });
-        
+
+         var tourButtonOptions = [
+            {
+                text: 'Back',
+                classes: 'shepherd-button-secondary',
+                action: tour.back,
+            },
+            {
+                text: 'Next',
+                classes: 'shepherd-button-primary',
+                action: tour.next,
+            },
+            {
+                text: 'Exit',
+                classes: 'shepherd-button-primary',
+                action: function () {
+                    return tour.cancel();
+                }
+            }
+        ]
+
         tour.addStep('tour',{
             title: "Heading",
             text: "Your website name",
             attachTo: ".heading-shepherd right",
-            buttons: [
-                {
-                    text: 'Back',
-                    classes: 'shepherd-button-secondary',
-                    action: tour.back,
-                },
-                {
-                    text: 'Next',
-                    classes: 'shepherd-button-primary',
-                    action: tour.next,
-                },
-                {
-                    text: 'Exit',
-                    classes: 'shepherd-button-primary',
-                    action: function(){
-                        return tour.hide();
-                    }
-                }
-            ]
+            buttons: tourButtonOptions,
         });
 
         tour.addStep('tour',{
             title: "Brand Name",
             text: "Your brand logo",
             attachTo: ".brand-shepherd right",
-            buttons: [
-                {
-                    text: 'Back',
-                    classes: 'shepherd-button-secondary',
-                    action: tour.back,
-                },
-                {
-                    text: 'Next',
-                    classes: 'shepherd-button-primary',
-                    action: tour.next,
-                },
-                {
-                    text: 'Exit',
-                    classes: 'shepherd-button-primary',
-                    action: function(){
-                        return tour.hide();
-                    }
-                }
-            ]
+            buttons: tourButtonOptions,
         });
 
         tour.addStep('tour',{
             title: "Home",
             text: "Your companies portfolio and establishment",
             attachTo: ".home-shepherd bottom",
-            buttons: [
-                {
-                    text: 'Back',
-                    classes: 'shepherd-button-secondary',
-                    action: tour.back,
-                },
-                {
-                    text: 'Next',
-                    classes: 'shepherd-button-primary',
-                    action: tour.next,
-                },
-                {
-                    text: 'Exit',
-                    classes: 'shepherd-button-primary',
-                    action: function(){
-                        return tour.hide();
-                    }
-                }
-            ]
+            buttons: tourButtonOptions,
         });
 
         tour.addStep('tour',{
             title: "Information",
             text: "Yours companies operation and explaining how it operates",
             attachTo: ".information-shepherd bottom",
-            buttons: [
-                {
-                    text: 'Back',
-                    classes: 'shepherd-button-secondary',
-                    action: tour.back,
-                },
-                {
-                    text: 'Next',
-                    classes: 'shepherd-button-primary',
-                    action: tour.next,
-                },
-                {
-                    text: 'Exit',
-                    classes: 'shepherd-button-primary',
-                    action: function(){
-                        return tour.hide();
-                    }
-                }
-            ]
+            buttons: tourButtonOptions,
         });
 
         tour.addStep('tour',{
             title: "Contact",
             text: "You explain how clients or customers can contact you to get your service",
             attachTo: ".contact-shepherd bottom",
-            buttons: [
-                {
-                    text: 'Back',
-                    classes: 'shepherd-button-secondary',
-                    action: tour.back,
-                },
-                {
-                    text: 'Next',
-                    classes: 'shepherd-button-primary',
-                    action: tour.next,
-                },
-                {
-                    text: 'Exit',
-                    classes: 'shepherd-button-primary',
-                    action: function(){
-                        return tour.hide();
-                    }
-                }
-            ]
+            buttons: tourButtonOptions,
         });
 
         $scope.startGuide = function(){
